c01a586baf916debfb86d2c3905ffd4b56370d8e
yacine bouklif <y.bouklif@it-students.fr>
2024-08-18 13:49:34 +0200

    Correction durée SetTimeout pour l'UX

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,35 +1,9 @@
-// let texte = document.querySelector(".js-texte");
-// let email = document.querySelector(".js-email");
-// let message = document.querySelector(".js-message");
-// let nom = document.querySelector(".js-nom");
-// let bouton = document.querySelector(".js-bouton");
-// const emailRegex =
-//   /^[a-zA-Z0-9][a-zA-Z0-9!#$%&'*+\-/=?^_`{|}~]*[a-zA-Z0-9]@[a-zA-Z0-9]+\.[a-zA-Z]+$/;
-
-// bouton.addEventListener("click", function () {
-//   if (email.value === "" || message.value === "" || nom.value === "") {
-//     texte.innerHTML = "Rentrez vos informations avant de valider ! ";
-//     texte.classList.add("text-red-500", "font-bold");
-//     setTimeout(function () {
-//       texte.innerHTML = "";
-//     }, 5000);
-
-//   } else {
-//     texte.innerHTML = "Merci pour votre confiance. ";
-//     texte.classList.add("text-blue-600", "font-bold");
-//     setTimeout(function () {
-//       texte.innerHTML = "";
-//     }, 5000);
-//   }
-// });
-
 let texte = document.querySelector(".js-texte");
 let email = document.querySelector(".js-email");
 let message = document.querySelector(".js-message");
 let nom = document.querySelector(".js-nom");
 let bouton = document.querySelector(".js-bouton");
 
-// Regex complexe pour la validation de l'email
 const emailRegex =
   /^[a-zA-Z0-9][a-zA-Z0-9!#$%&'*+\-/=?^_`{|}~]*[a-zA-Z0-9]@[a-zA-Z0-9]+\.[a-zA-Z]+$/;
 
@@ -41,22 +15,19 @@ bouton.addEventListener("click", function () {
     !emailRegex.test(email.value)
   ) {
     texte.innerHTML = "Rentrez vos informations avant de valider !";
+    texte.classList.add("text-red-500", "font-bold");
     if (email.value !== "" && !emailRegex.test(email.value)) {
       texte.innerHTML = "Veuillez entrer une adresse email valide.";
     }
-
-    texte.classList.add("text-red-500", "font-bold");
     setTimeout(function () {
       texte.innerHTML = "";
-      texte.classList.remove("text-red-500", "font-bold");
-    }, 5000);
+    }, 4500);
   } else {
     texte.innerHTML = "Merci pour votre confiance.";
     texte.classList.remove("text-red-500", "font-bold");
-    texte.classList.add("text-green-500");
+    texte.classList.add("text-green-500", "font-bold");
     setTimeout(function () {
       texte.innerHTML = "";
-      texte.classList.remove("text-green-500");
-    }, 5000);
+    }, 3000);
   }
 });
